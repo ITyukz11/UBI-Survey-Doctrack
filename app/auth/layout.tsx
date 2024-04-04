c82b8eb88c5880e0ef6fa152dcd164dd133d98e9
Errol Robyn Abella <it.era0211@gmail.com>
2024-04-04 16:04:13 +0800

    Added home button in login form and register form

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,12 @@
 'use client'
+import { Label } from '@/components/ui/label'
 import { AnimatePresence, motion} from 'framer-motion'
+import { useRouter } from 'next/navigation'
 import React from 'react'
+import {  FaLongArrowAltLeft } from 'react-icons/fa'
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter()
   return (
     <AnimatePresence>
       <motion.div
@@ -12,10 +16,17 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0.5 }}
       >
-        <div className='h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat min-h-screen'
+        <div className='h-screen flex items-center justify-center flex-col bg-cover bg-center bg-no-repeat min-h-screen'
           style={{ backgroundImage: "url('/background.png')" }}>
+              <Label className=' cursor-pointer transition-transform duration-300 ease-in-out transform hover:-translate-x-2' onClick={()=> router.push('/')}>
+     
+     <p className='text-white flex flex-row mb-4 gap-2 w-96'> <FaLongArrowAltLeft /> Back</p>
+      </Label>
           {children}
+   
+
         </div>
+    
       </motion.div>
     </AnimatePresence>
   )
